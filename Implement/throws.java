9f8504edf56762836a47ec76915e2080bf87b6bb
gelong25 <Hei2@naver.com>
2024-06-15 00:07:13 +0900

    절약 문제 수정

--- Implement/throws.java
+++ Implement/throws.java
@@ -1,39 +1,47 @@
 import java.io.*;
+
 class Main {
     public static void main (String[] args) throws Exception {
+        // BufferedReader를 사용하여 입력을 받을 준비를 함
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String input = br.readLine();
         
+        // 첫 번째 줄의 입력을 받아 거래 건수로 변환
+        String input = br.readLine();
         int size = input.charAt(0) - '0';
         
+        // 거래 정보를 저장할 배열 선언
         String[] info = new String[size];
+        
+        // 거래 정보 입력받기
         for (int i = 0; i < size; i++) {
-                info[i] = br.readLine();
-            
+            info[i] = br.readLine();    
         }
 
-        int cnt = 0;
-        String result; 
+        int balance = 0;  // 현재 잔액을 저장할 변수
+        String result = "success";  // 결과값을 저장할 변수, 기본값은 "success"
 
+        // 각 거래를 순서대로 처리
         for (int i = 0; i < size; i++) {
+            // 공백을 기준으로 거래명을 분리하여 배열에 저장
             String[] sep = info[i].split(" ");
-            String ci = sep[0];
+            String ci = sep[0];  // 거래명 (in / out)
+            int vi = Integer.parseInt(sep[1]);  // 거래액
 
-            int vi = Integer.parseInt(sep[1]);
-
-
+            // 거래명에 따라 잔액을 조정
             if (ci.equals("in")) {
-                cnt += vi;
+                balance += vi;  // 수입인 경우 잔액을 증가
             } else {
-                cnt -= vi;
+                balance -= vi;  // 지출인 경우 잔액을 감소
+
+                // 잔액이 음수이면 실패 처리
+                if (balance < 0) {
+                    result = "fail";
+                    break;  // 더 이상 처리하지 않고 루프 종료
+                }
             }
         }
-        if (cnt > 0) {
-            result = "Success";
-        } else {
-            result = "fail";
-        }
-        
-    System.out.println(result);    
+
+        // 결과 출력
+        System.out.println(result);    
     }
 }
