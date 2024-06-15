dfa52b2cbb2d7c59f010e055cb4eaf2d8b784454
gelong25 <Hei2@naver.com>
2024-06-15 11:15:32 +0900

    뱀이 지나간 자리  수정

--- Implement/snake.java
+++ Implement/snake.java
@@ -29,30 +29,29 @@ public class snake {
             } 
             else {
                 // 왼쪽으로 이동 후 이동경로 #으로 표시 
-                for (int j = M-1; j >= 0; j--) {
+                for (int j = M - 1; j >= 0; j--) {
                     map[i][j] = '#';
                 }
                 // 다시 오른쪽으로 이동 
                 moveRight = true;
             }
 
-             // N을 넘지 않는 경우 2행 아래로 이동 
-             if (i + 2 < N) {
-                i++; // 현재 이동을 마친 후 한 행을 더 내려감
-                // 내려간 행은 오른쪽으로 이동할지, 왼쪽으로 이동할지에 따라 다른 방향으로 움직임
+            // N을 넘지 않는 경우 2행 아래로 이동 
+            i += 2; 
+            if (i < N) {
                 if (moveRight) {
                     for (int j = 0; j < M; j++) {
                         map[i][j] = '#';
                     }
-                    moveRight = false; // 다음번에는 왼쪽으로 이동
                 } else {
                     for (int j = M - 1; j >= 0; j--) {
                         map[i][j] = '#';
                     }
-                    moveRight = true; // 다음번에는 오른쪽으로 이동
                 }
+                moveRight = !moveRight; // 이동 방향 전환
             }
         }
+        
         
         // 결과 출력
         for (int i = 0; i < N; i++) {
